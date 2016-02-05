6302e0fb7f999c82bd2ba5cf3ed7b8eb07c05257
Jonathan Ginn <jon@wearebase.com>
2016-02-05 15:16:44 +0000

    Add check for Timber Class, as these files require Timber

--- php/breadcrumbs.php
+++ php/breadcrumbs.php
@@ -1,5 +1,13 @@
 <?php
 
+if (!class_exists('Timber')) {
+    add_action('admin_notices', function () {
+            echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="'.esc_url(admin_url('plugins.php#timber')).'">'.esc_url(admin_url('plugins.php')).'</a></p></div>';
+        });
+
+    return;
+}
+
 function base_getBreadcrumbs() {
 
   if (is_404()) {
